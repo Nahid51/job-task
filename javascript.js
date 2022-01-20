aa5cf497cb3e80cc397521a4cf1a73cf0e71abcd
Nahid51 <hasannahidnahid@gmail.com>
2022-01-21 02:43:56 +0600

    button modification using js

--- javascript.js
+++ javascript.js
@@ -9,3 +9,19 @@ toggleMenu.addEventListener('click', function () {
 closeMenu.addEventListener('click', function () {
     navMenu.classList.remove('show')
 })
+
+document.getElementById('contact-button').addEventListener('click', function () {
+    alert('Document will be coming soon.')
+})
+
+document.getElementById('loadMoreButton').addEventListener('click', function () {
+    alert('Document will be coming soon.')
+})
+
+document.getElementById('formSubmitButton').addEventListener('click', function () {
+    alert('Document will be coming soon.')
+})
+
+document.getElementById('contactNowButton').addEventListener('click', function () {
+    alert('Document will be coming soon.')
+})
